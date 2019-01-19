4461f4bb06e1d41e71198a540d0d5928dc64e893
Dosbol Duysekov <dosbol98@gmail.com>
2019-01-19 20:10:23 +0600

    Initial commit with some views

---
+++ Shopify Challenge/CustomCollectionsListPageController.swift
@@ -0,0 +1,68 @@
+//
+//  CustomCollectionsListPageController.swift
+//  Shopify Challenge
+//
+//  Created by Dosbol Duysekov on 1/19/19.
+//  Copyright © 2019 Dosbol Duysekov. All rights reserved.
+//
+
+import UIKit
+
+class CustomCollectionsListPageController: UIViewController {
+    
+    // MARK: View Elements
+    
+    lazy var collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "CollectionListCellID")
+        collectionView.backgroundColor = .clear
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        return collectionView
+    }()
+    
+    // MARK: Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupViews()
+    }
+    
+    // MARK: Custom Methods
+    
+    private func setupViews() {
+        view.backgroundColor = UIColor(red: 0.13, green: 0.17, blue: 0.21, alpha: 1.0)
+        view.addSubview(collectionView)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        collectionView.topAnchor.constraint(equalTo: view.topAnchor, constant: 16).isActive = true
+        collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -16).isActive = true
+        collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
+        collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
+    }
+}
+
+// MARK: CollectionView Delegates and Data Source
+
+extension CustomCollectionsListPageController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CollectionListCellID", for: indexPath)
+        cell.backgroundColor = UIColor(red: 0.57, green: 0.62, blue: 0.67, alpha: 1.0)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 32
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let size = collectionView.frame.width / 3 - 16
+        return CGSize(width: size, height: size)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 32
+    }
+}
