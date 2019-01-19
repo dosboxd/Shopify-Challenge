349a197d6f5a92fa8a7d85c015076c7b36e6c13b
Dosbol Duysekov <dosbol98@gmail.com>
2019-01-19 19:24:05 +0600

    Initial Commit

---
+++ Shopify Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Shopify Challenge
+//
+//  Created by Dosbol Duysekov on 1/19/19.
+//  Copyright © 2019 Dosbol Duysekov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
